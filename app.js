def7860dc04a70394f58e1b265725eb7b5ad4761
Elias Angulo <emangulo1@gmail.com>
2024-05-31 19:10:23 -0700

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config;
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT | 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("It works!");
